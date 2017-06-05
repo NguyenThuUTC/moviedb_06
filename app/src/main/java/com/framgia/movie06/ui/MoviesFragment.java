2430926d7f0cd62dead14ff8dd1a352a4bb924fe
NguyenThuUTC <nguyenthu290895@gmail.com>
2017-06-05 14:53:56 +0700

    create home ui 1

---
+++ app/src/main/java/com/framgia/movie06/ui/MoviesFragment.java
@@ -0,0 +1,25 @@
+package com.framgia.movie06.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.framgia.movie06.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MoviesFragment extends Fragment {
+    public MoviesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movies, container, false);
+    }
+}
