39933542b53b3481afde7846c0e7584f8afd10c0
NguyenThuUTC <nguyenthu290895@gmail.com>
2017-06-12 14:44:10 +0700

    loaddata

--- app/src/main/java/com/framgia/movie06/ui/activity/SearchActivity.java
+++ app/src/main/java/com/framgia/movie06/ui/activity/SearchActivity.java
@@ -3,26 +3,126 @@ package com.framgia.movie06.ui.activity;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 import com.framgia.movie06.R;
+import com.framgia.movie06.adapter.MovieAdapter;
+import com.framgia.movie06.data.model.Movie;
+import com.framgia.movie06.data.model.MoviesResponse;
+import com.framgia.movie06.service.Config;
+import com.framgia.movie06.service.SearchService;
+import java.util.ArrayList;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SearchActivity extends AppCompatActivity {
     public static final String SEARCH = "Search";
+    private Retrofit mRetrofit;
+    private SearchService mSearchService;
+    private MovieAdapter mMovieAdapter;
+    private List<Movie> mMovieList;
+    private int mPage = 1;
+    private int mTotalPage;
+    public static final String LOAD_ERROR = "error";
+    private String mQuerySearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        setTitle(SEARCH);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        initViews();
+    }
+
+    private void initViews() {
+        RecyclerView recyclerSearch = (RecyclerView) findViewById(R.id.recycler_search);
+        mRetrofit = new Retrofit.Builder().baseUrl(Config.API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        mSearchService = mRetrofit.create(SearchService.class);
+        mMovieList = new ArrayList<>();
+        mMovieAdapter = new MovieAdapter(mMovieList);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerSearch.setLayoutManager(layoutManager);
+        recyclerSearch.setAdapter(mMovieAdapter);
+        recyclerSearch.setOnScrollListener(mScrollRecyclerSearch);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.search, menu);
         MenuItem item = menu.findItem(R.id.menu_search);
-        setTitle(SEARCH);
         SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+        searchView.setOnQueryTextListener(mTextChanged);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private SearchView.OnQueryTextListener mTextChanged = new SearchView.OnQueryTextListener() {
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+            loadSearchFeatures(mPage, query);
+            mQuerySearch = query;
+            return false;
+        }
+
+        @Override
+        public boolean onQueryTextChange(String s) {
+            return false;
+        }
+    };
+
+    private RecyclerView.OnScrollListener mScrollRecyclerSearch =
+            new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                }
+
+                @Override
+                public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                    final LinearLayoutManager linearLayoutManager =
+                            (LinearLayoutManager) recyclerView.getLayoutManager();
+                    int visibleItemCount = linearLayoutManager.getChildCount();
+                    int totalItemCount = linearLayoutManager.getItemCount();
+                    int firstVisibleItemPosition =
+                            linearLayoutManager.findFirstVisibleItemPosition();
+                    if (mPage <= mTotalPage
+                            && (visibleItemCount + firstVisibleItemPosition) >= totalItemCount
+                            && firstVisibleItemPosition >= 0) {
+                        mPage++;
+                        loadSearchFeatures(mPage, mQuerySearch);
+                    }
+                }
+            };
+
+    private void loadSearchFeatures(int page, String query) {
+        mMovieList.clear();
+        mSearchService.getSearchMovieFeature(Config.API_KEY, query, page)
+                .enqueue(new Callback<MoviesResponse>() {
+                    @Override
+                    public void onResponse(Call<MoviesResponse> call,
+                            Response<MoviesResponse> response) {
+                        if (response == null || response.body() == null ||
+                                response.body().getResults() == null) {
+                            return;
+                        }
+                        mMovieList.addAll(response.body().getResults());
+                        mTotalPage = response.body().getTotalPages();
+                        mMovieAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onFailure(Call<MoviesResponse> call, Throwable t) {
+                        Toast.makeText(SearchActivity.this, LOAD_ERROR, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
